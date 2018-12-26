1619d9e36c5cb5eb7aaa96ca2c6093d619938554
jaurenigel <jaurenigel@gmail.com>
2018-12-26 10:53:14 +0200

    added morgan middleware, added body-parser, passing the body and handling CORS errors

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // routes which handles request
 app.use("/products", productRoutes);
